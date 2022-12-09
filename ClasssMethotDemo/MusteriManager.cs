574aeecc47cf7b0e3586bf0791bbce97680e7d36
Tuğba Yazıcı <106247691+tugbayzc@users.noreply.github.com>
2022-12-09 14:26:41 +0300

    Add files via upload

---
+++ ClasssMethotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasssMethotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " Müşteri eklendi!");
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id);
+            Console.WriteLine(musteri.Ad);
+            Console.WriteLine(musteri.Soyad);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " Müşteri silindi!");
+        }
+    }
+}
---
+++ ClasssMethotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClasssMethotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            {
+                musteri1.Id = 1;
+                musteri1.Ad = "Tuğba";
+                musteri1.Soyad = "Yazıcı";
+            }
+            Musteri musteri2 = new Musteri();
+            {
+                musteri2.Id = 2;
+                musteri2.Ad = "Emel";
+                musteri2.Soyad = "Yazıcı";
+            }
+            Musteri musteri3 = new Musteri();
+            {
+                musteri3.Id = 3;
+                musteri3.Ad = "Tuncel";
+                musteri3.Soyad = "Yazıcı";
+            }
+
+            Musteri[] musteriler = new Musteri[3] { musteri1, musteri2, musteri3 };
+            MusteriManager musteriIslem = new MusteriManager();
+            
+            Console.WriteLine("--ekle--");
+            musteriIslem.Ekle(musteri1);
+            
+            Console.WriteLine("--listele--");
+            musteriIslem.Listele(musteri2);
+            
+            Console.WriteLine("--sil--");
+            musteriIslem.Sil(musteri3);
+        }
+    }
+}
